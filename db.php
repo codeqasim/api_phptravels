ee2fb0fe51b6b3122779b6123eb0f5f3b632e9ae
Qasim <adeel@tecfare.com>
2022-09-12 06:25:17 +0500

    initial push

---
+++ db.php
@@ -0,0 +1,39 @@
+<?php
+
+use Medoo\Medoo;
+
+header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+
+$whitelist = array( '127.0.0.1', '::1' );
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){ 
+$host = "localhost";
+} else {
+$host = "api-4b8a059e84";
+}
+
+$servername = $host;
+$database = "production_api_database";
+$username = "api_database";
+$password = "api_database";
+
+// CREATE CONNECTION
+$mysqli = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+  $dbConnection = "MySQL Not Connected";
+} else {
+  $dbConnection = "MySQL Connected";
+}
+// echo "Connected successfully";
+
+// Connect the database.
+$db = new Medoo([
+  'type' => 'mysql',
+  'host' => $host,
+  'database' => 'production_api_database',
+  'username' => 'api_database',
+  'password' => 'api_database'
+]);
