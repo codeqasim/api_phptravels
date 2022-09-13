90a6ab844dfbac12715f833ac5a927f89ef0af57
Qasim <adeel@tecfare.com>
2022-09-14 02:16:56 +0500

    Update app.php

--- routes/app.php
+++ routes/app.php
@@ -9,35 +9,13 @@ $router->post('app', function() {
     include "./config.php";
 
     // VALIDATION
-    required('email'); required('password');
+    required('user_id'); 
 
-    $data = $db->select("users","*", [
-        "email" => $_POST['email'],
-        "password" => md5($_POST['password']),
+    $data = $db->select("settings","*", [
+        "user_id" => $_POST['user_id'],
     ]);
 
-    if(isset($data[0])) { 
-    
-        $user_data = (object)$data[0]; $respose = array ( "status"=> true, "message"=>"user details", "data"=> $user_data );
-
-        // SAVE ACTIVITY LOGS
-        $log_user_id = $user_data->id;
-        $log_type = "login";
-        $log_desc = $user_data;
-
-        // LOG SEND EMAIL
-        $log_mail = true;
-        $log_mail_name = $user_data->first_name;
-        $log_send_email = $user_data->email;
-        $log_mail_subject = "User Login";
-        $log_mail_title = "Account Login";
-        $log_mail_content = "This is to inform you that you just logged at our portal with IP ". get_client_ip();
-        
-        include "./logs.php";
-
-    } else {
-    $respose = array ( "status"=>false, "message"=>"no user found", "data"=> null );
-}
+    $respose = array ( "status"=>true, "message"=>"user app details", "data"=> $data );
 
 echo json_encode($respose);
 
