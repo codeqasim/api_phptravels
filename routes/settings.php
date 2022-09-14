3aaf75078bd5da33e0804842f45f988ff672c920
Qasim <adeel@tecfare.com>
2022-09-14 07:22:59 +0500

    Update settings.php

--- routes/settings.php
+++ routes/settings.php
@@ -26,9 +26,11 @@ $router->post('settings', function() {
     $val = "social_twitter"; if(isset($_POST[$val]) || !empty($_POST[$val])) { $data[$val] = $_POST[$val]; }
     $val = "social_linkedin"; if(isset($_POST[$val]) || !empty($_POST[$val])) { $data[$val] = $_POST[$val]; }
     $val = "social_instagram"; if(isset($_POST[$val]) || !empty($_POST[$val])) { $data[$val] = $_POST[$val]; }
-    $val = "social_google"; if(isset($_POST[$val]) || !empty($_POST[$val])) { $data[$val] = $_POST[$val]; }
+    $val = "social_whatsapp"; if(isset($_POST[$val]) || !empty($_POST[$val])) { $data[$val] = $_POST[$val]; }
  
-    
+     // VALIDATION
+     required('user_id');
+
     // UPDATE SETTINGS
     $data = $db->update("settings", $data , [ "user_id" => $_POST['user_id'], ]);
 
